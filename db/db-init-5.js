6dc00f637d07254f37ee6a2e0f0a6624e0b6b788
red <red54321@outlook.com>
2020-12-10 15:43:36 +0000

    Add /topics/<id>/subtopics into swagger and update model
    1. Add /topics/<id>/subtopics into swagger
    2. Update Topic model.
    3. Add new mongo script to init topic collection with small tree structure that has no effect on existing code.

---
+++ db/db-init-5.js
@@ -0,0 +1,181 @@
+// Init topics database with 5 documents for testing
+// in next, current format and putting into index: 'id'.
+// These are aranged to link in a tree structure.
+
+db = db.getSiblingDB('topics')
+
+db.topics.remove({})
+
+db.topics.insertOne({
+    "id" : "1", 
+    "next" : {
+        "_id" : "1",
+        "description" : "next test description - 1",
+        "title" : "test title - 1",
+        "keywords" : [ "keyword 1", "keyword 2", "keyword 3" ],
+        "state" : "in_progress",
+        "links": {
+            "self": {
+                "href": "http://localhost:8080/topics/1",
+                "id": "1"
+            },
+            "subtopics": {
+                 "href": "http://localhost:8080/topics/1/subtopics",
+            },
+            "content": {
+                "href": "http://localhost:8080/topics/1/content",
+            }
+        },
+        "subtopics" : [
+            "2",
+            "3"
+        ]
+    },
+    "current" : {
+        "_id" : "1",
+        "description" : "current test description - 1",
+        "title" : "test title - 1",
+        "keywords" : [ "keyword 1", "keyword 2", "keyword 3" ],
+        "state" : "published",
+        "links": {
+            "self": {
+                "href": "http://localhost:8080/topics/1",
+                "id": "1"
+            },
+            "subtopics": { 
+                "href": "http://localhost:8080/topics/1/subtopics",
+            },
+            "content": {
+                "href": "http://localhost:8080/topics/1/content",
+            }
+        },
+        "subtopics" : [
+            "2",
+            "3"
+        ]
+    }
+})
+
+db.topics.insertOne({
+    "id" : "2", 
+    "next" : {
+        "_id" : "2",
+        "description" : "next test description - 2",
+        "title" : "test title - 2",
+        "keywords" : [ "keyword 1", "keyword 2", "keyword 3" ],
+        "state" : "in_progress",
+        "links": {
+            "self": {
+                "href": "http://localhost:8080/topics/2",
+                "id": "2"
+            },
+            "subtopics": {
+                 "href": "http://localhost:8080/topics/2/subtopics",
+            },
+            "content": {
+                "href": "http://localhost:8080/topics/2/content",
+            }
+        },
+        "subtopics" : [
+            "4"
+        ]
+    },
+    "current" : {
+        "_id" : "2",
+        "description" : "current test description - 2",
+        "title" : "test title - 2",
+        "keywords" : [ "keyword 1", "keyword 2", "keyword 3" ],
+        "state" : "published",
+        "links": {
+            "self": {
+                "href": "http://localhost:8080/topics/2",
+                "id": "2"
+            },
+            "subtopics": { 
+                "href": "http://localhost:8080/topics/2/subtopics",
+            },
+            "content": {
+                "href": "http://localhost:8080/topics/2/content",
+            }
+        },
+        "subtopics" : [
+            "4"
+        ]
+    }
+})
+
+db.topics.insertOne({
+    "id" : "3", 
+    "next" : {
+        "_id" : "3",
+        "description" : "next test description - 3",
+        "title" : "test title - 3",
+        "keywords" : [ "keyword 1", "keyword 2", "keyword 3" ],
+        "state" : "in_progress",
+        "links": {
+            "self": {
+                "href": "http://localhost:8080/topics/3",
+                "id": "3"
+            },
+            "content": {
+                "href": "http://localhost:8080/topics/3/content",
+            }
+        }
+    },
+    "current" : {
+        "_id" : "3",
+        "description" : "current test description - 3",
+        "title" : "test title - 3",
+        "keywords" : [ "keyword 1", "keyword 2", "keyword 3" ],
+        "state" : "published",
+        "links": {
+            "self": {
+                "href": "http://localhost:8080/topics/3",
+                "id": "3"
+            },
+            "content": {
+                "href": "http://localhost:8080/topics/3/content",
+            }
+        }
+    }
+})
+
+db.topics.insertOne({
+    "id" : "4", 
+    "next" : {
+        "_id" : "4",
+        "description" : "next test description - 4",
+        "title" : "test title - 4",
+        "keywords" : [ "keyword 1", "keyword 2", "keyword 3" ],
+        "state" : "in_progress",
+        "links": {
+            "self": {
+                "href": "http://localhost:8080/topics/4",
+                "id": "4"
+            },
+            "content": {
+                "href": "http://localhost:8080/topics/4/content",
+            }
+        }
+    },
+    "current" : {
+        "_id" : "4",
+        "description" : "current test description - 4",
+        "title" : "test title - 4",
+        "keywords" : [ "keyword 1", "keyword 2", "keyword 3" ],
+        "state" : "published",
+        "links": {
+            "self": {
+                "href": "http://localhost:8080/topics/4",
+                "id": "4"
+            },
+            "content": {
+                "href": "http://localhost:8080/topics/4/content",
+            }
+        }
+    }
+})
+
+db.topics.find().forEach(function(doc) {
+    printjson(doc);
+})
