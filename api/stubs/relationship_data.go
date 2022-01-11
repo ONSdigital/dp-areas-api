5902d53cecb569df8c5feead467b409898f3f10e
Mariana Silva <Marianasilvago1234@gmail.com>
2022-01-11 21:29:53 +0530

    stub few unitary authorities as related areas for wales

--- api/stubs/relationship_data.go
+++ api/stubs/relationship_data.go
@@ -14,5 +14,14 @@ var Relationships = map[string][]models.AreaRelationShips{
 		{"E12000008", "South East", "/v1/area/E12000008"},
 		{"E12000009", "South West", "/v1/area/E12000009"},
 	},
-	"W92000004": make([]models.AreaRelationShips, 0),
+	"W92000004": {
+		{"W06000001", "Isle of Anglesey", "/v1/area/W06000001"},
+		{"W06000002", "Gwynedd", "/v1/area/W06000002"},
+		{"W06000003", "Conwy", "/v1/area/W06000003"},
+		{"W06000004", "Denbighshire", "/v1/area/W06000004"},
+		{"W06000005", "Flintshire", "/v1/area/W06000005"},
+		{"W06000006", "Wrexham", "/v1/area/W06000006"},
+		{"W06000008", "Ceredigion", "/v1/area/W06000008"},
+		{"W06000009", "Pembrokeshire", "/v1/area/W06000009"},
+	},
 }
