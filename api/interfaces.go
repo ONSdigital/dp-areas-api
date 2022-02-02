365bc69b99172dd9754f731c0e387d2bc6b045ef
Peter Idah <peter.idah@gmail.com>
2022-02-02 02:16:10 +0000

    add ancestry (stubbed)

--- api/interfaces.go
+++ api/interfaces.go
@@ -8,6 +8,7 @@ import (
 )
 
 //go:generate moq -out mock/areaStore.go -pkg mock . AreaStore
+//go:generate moq -out mock/ancestorStore.go -pkg mock . AncestorStore
 
 // AreaStore represents all the required methods from mongo DB
 type AreaStore interface {
@@ -16,4 +17,9 @@ type AreaStore interface {
 	GetArea(ctx context.Context, id string) (*models.Area, error)
 	GetVersion(ctx context.Context, id string, versionID int) (*models.Area, error)
 	GetAreas(ctx context.Context, offset, limit int) (*models.AreasResults, error)
+}
+
+// Ancestors defines a method to lookup ancestors from an areaID
+type AncestorStore interface {
+	GetAncestors(areaID string) ([]models.AreasAncestors, error)
 }
--- go.mod
+++ go.mod
@@ -30,7 +30,6 @@ require (
 	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.9.1 // indirect
-	github.com/jackc/pgx v3.6.2+incompatible // indirect
 	github.com/jackc/puddle v1.2.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
