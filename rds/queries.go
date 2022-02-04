cd83f7ec54b02cc018e038f4a828490bff4fd853
Mariana Silva <Marianasilvago1234@gmail.com>
2022-02-04 18:09:56 +0530

    fix relationships query

--- rds/queries.go
+++ rds/queries.go
@@ -3,5 +3,5 @@ package rds
 const (
 	getBasicArea      = "select id, code, active from areas_basic where id=$1"
 	getAreaCode       = "select code from area where code = $1"
-	getRelationShipAreas = "select code, name from area, area_relationship where rel_area_code = code and area_code = $1"
+	getRelationShipAreas = "select area_code, name from area_name, area_relationship where rel_area_code = area_code and area_code = $1"
 )
