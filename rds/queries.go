e46da9901494ca3cf7f89b999e6a6af655959cc4
Mariana Silva <Marianasilvago1234@gmail.com>
2022-02-07 22:34:01 +0530

    fix fetch relations query

--- rds/queries.go
+++ rds/queries.go
@@ -3,5 +3,5 @@ package rds
 const (
 	getBasicArea      = "select id, code, active from areas_basic where id=$1"
 	getAreaCode       = "select code from area where code = $1"
-	getRelationShipAreas = "select area_code, name from area_name, area_relationship where rel_area_code = area_code and area_code = $1"
+	getRelationShipAreas = "select an.area_code, an.name from area_name as an, area_relationship as ar where ar.rel_area_code = an.area_code and ar.area_code =$1"
 )
