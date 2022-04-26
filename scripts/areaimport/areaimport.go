9de1b0761786f8832c53fa79f42f36e8a0d88288
Mariana Silva <Marianasilvago1234@gmail.com>
2022-04-26 23:30:33 +0530

    change history csv data import into area import api

--- scripts/areaimport/areaimport.go
+++ scripts/areaimport/areaimport.go
@@ -1,128 +1,114 @@
 package main
 
 import (
-    "bufio"
-    "bytes"
-    "encoding/csv"
-    "encoding/json"
-    "fmt"
-    "io"
-    "log"
-    "net/http"
-    "os"
-    "time"
+	"bufio"
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
-    "github.com/ONSdigital/dp-areas-api/models"
+	"github.com/ONSdigital/dp-areas-api/models"
 )
 
 var areaTypeAndCode = map[string]string{
-    "E92": "Country",
-    "E06": "Unitary Authorities",
-    "W92": "Country",
-    "E12": "Region",
-    "W06": "Unitary Authorities",
-    "E47": "Combined Authorities",
-    "E11": "Metropolitan Counties",
-    "E10": "Counties",
-    "E09": "London Boroughs",
-    "E08": "Metropolitan Districts",
-    "E07": "Non-metropolitan Districts",
-    "E05": "Electoral Wards",
-    "W05": "Electoral Wards",
-}
-
-type logs struct {
-    errors  []string
-    success []string
+	"E92": "Country",
+	"E06": "Unitary Authorities",
+	"W92": "Country",
+	"E12": "Region",
+	"W06": "Unitary Authorities",
+	"E47": "Combined Authorities",
+	"E11": "Metropolitan Counties",
+	"E10": "Counties",
+	"E09": "London Boroughs",
+	"E08": "Metropolitan Districts",
+	"E07": "Non-metropolitan Districts",
+	"E05": "Electoral Wards",
+	"W05": "Electoral Wards",
 }
 
 func BoolPointer(b bool) *bool {
-    return &b
+	return &b
 }
 
-func importChangeHistoryAreaInfo() logs {
-    var errors []string
-    var success []string
-    csvFile, _ := os.Open("/Users/indra/Docs/ons/Code_History_Database_(December_2021)_UK/ChangeHistory.csv")
-    reader := csv.NewReader(bufio.NewReader(csvFile))
+func importChangeHistoryAreaInfo() {
+	csvFile, _ := os.Open("/Users/indra/Docs/ons/Code_History_Database_(December_2021)_UK/ChangeHistory.csv")
+	reader := csv.NewReader(bufio.NewReader(csvFile))
 
-    for {
-        line, err := reader.Read()
-        if err == io.EOF {
-            break
-        } else if err != nil {
-            log.Fatal(err)
-        }
-        if line[0] == "GEOGCD" {
-            continue
-        }
-        if line[10] != "live" {
-            continue
-        }        
-        if line[1] == "" {
-            errors = append(errors, "found area info with empty name: "+line[0])
-            continue
-        }
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		if line[0] == "GEOGCD" {
+			continue
+		}
+		if line[10] != "live" {
+			continue
+		}
 
-        _, areaTypeFound := areaTypeAndCode[line[8]]
-        if !areaTypeFound {
-            errors = append(errors, "area type not drive for: "+line[0])
-            continue
-        }
+		if line[1] == "" {
+			log.Printf("found area info with empty name: %+v", line[0])
+			continue
+		}
 
-        const layout = "02/01/2006 15:04:05"
-        active_from, err := time.Parse(layout, line[5])
-        if err != nil {
-            log.Fatal("Could not parse time:", err)
-        }
-        active_to, _ := time.Parse(layout, line[6])
+		_, areaTypeFound := areaTypeAndCode[line[8]]
+		if !areaTypeFound {
+			log.Printf("area type not drive for: %+v", line[0])
+			continue
+		}
 
-        areaName := models.AreaName{
-            Name:       line[1],
-            ActiveFrom: &active_from,
-            ActiveTo:   &active_to,
-        }
+		const layout = "02/01/2006 15:04:05"
+		active_from, err := time.Parse(layout, line[5])
+		if err != nil {
+			log.Fatal("Could not parse time:", err)
+		}
+		active_to, _ := time.Parse(layout, line[6])
 
-        areaInfo := models.AreaParams{
-            Code:         line[0],
-            AreaName:     &areaName,
-            Visible:      BoolPointer(true),
-            ActiveFrom:   &active_from,
-            ActiveTo:     &active_to,
-            ParentId:     line[7],
-            AreaHectares: line[11],
-        }
+		areaName := models.AreaName{
+			Name:       line[1],
+			ActiveFrom: &active_from,
+			ActiveTo:   &active_to,
+		}
 
-        json, err := json.Marshal(areaInfo)
+		areaInfo := models.AreaParams{
+			Code:         line[0],
+			AreaName:     &areaName,
+			Visible:      BoolPointer(true),
+			ActiveFrom:   &active_from,
+			ActiveTo:     &active_to,
+			ParentId:     line[7],
+			AreaHectares: line[11],
+		}
 
-        if err != nil {
-            log.Fatalf("json marshalling failed: %+v", err)
-        }
+		json, err := json.Marshal(areaInfo)
 
-        client := &http.Client{}
+		if err != nil {
+			log.Fatalf("json marshalling failed: %+v", err)
+		}
 
-        req, err := http.NewRequest(http.MethodPut, "http://127.0.0.1:25500/v1/areas/"+line[0], bytes.NewBuffer(json))
-        if err != nil {
-            log.Fatalf("request failed %+v", err)
-        }
+		client := &http.Client{}
 
-        req.Header.Set("Content-Type", "application/json")
-        resp, err := client.Do(req)
-        if err != nil {
-            log.Fatalf("error response from server: %+v", err)
-        }
+		req, err := http.NewRequest(http.MethodPut, "http://127.0.0.1:25500/v1/areas/"+line[0], bytes.NewBuffer(json))
+		if err != nil {
+			log.Fatalf("request failed %+v", err)
+		}
 
-        success = append(success, "api response for line[0]: "+resp.Status)
+		req.Header.Set("Content-Type", "application/json")
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatalf("error response from server: %+v", err)
+		}
 
-    }
-    logs := logs{
-        errors:  errors,
-        success: success,
-    }
-    return logs
+		log.Printf("api response is (%s): %+v \n", line[0], resp)
+
+	}
 }
 func main() {
-    result := importChangeHistoryAreaInfo()
-
-    fmt.Println(result)
+	importChangeHistoryAreaInfo()
 }
