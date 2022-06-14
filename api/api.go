9881b2611a72ed72f69e3c0124e60efbc35fd372
Nathan Shumoogum <nathan.shumoogum@methods.co.uk>
2022-06-14 11:21:56 +0100

    Wrap auth logic for web /navigation endpoint to handle noop for permissions auth

--- api/api.go
+++ api/api.go
@@ -59,6 +59,10 @@ func Setup(ctx context.Context, cfg *config.Config, router *mux.Router, dataStor
 
 		log.Info(ctx, "enabling only public endpoints for dataset api")
 		api.enablePublicEndpoints(ctx)
+
+		if cfg.EnablePermissionsAuth {
+			api.enablePublicEndpointsWithAuth(ctx)
+		}
 	}
 
 	return api
@@ -70,7 +74,10 @@ func (api *API) enablePublicEndpoints(ctx context.Context) {
 	api.get("/topics/{id}/subtopics", api.getSubtopicsPublicHandler)
 	api.get("/topics/{id}/content", api.getContentPublicHandler)
 	api.get("/topics", api.getTopicsListPublicHandler)
+}
 
+func (api *API) enablePublicEndpointsWithAuth(ctx context.Context) {
+	// api.get("/navigation", api.getNavigationPrivateHandler)
 	api.get(
 		"/navigation",
 		api.isAuthenticated(
