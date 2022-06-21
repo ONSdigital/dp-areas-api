e0e5eded65fe391f997ce7440e84dee0cf957493
Justin Pattarukuzhyil Jose <justin.p.jose@ons.gov.uk>
2022-06-21 18:06:12 +0100

    organise models

---
+++ models/navigation.go
@@ -0,0 +1,18 @@
+package models
+
+type Navigation struct {
+	Description string                 `json:"description"`
+	Links       *TopicLinks            `json:"links,omitempty"`
+	Items       *[]TopicNonReferential `json:"items,omitempty"`
+}
+
+// TopicNonReferential is used to create a single comprehensive list of topics and subtopics.
+type TopicNonReferential struct {
+	Description   string                 `json:"description,omitempty"`
+	Label         string                 `json:"label"`
+	Links         *TopicLinks            `json:"links,omitempty"`
+	Name          string                 `json:"name"`
+	SubtopicItems *[]TopicNonReferential `json:"subtopics,omitempty"`
+	Title         string                 `json:"title"`
+	Uri           string                 `json:"uri"`
+}
