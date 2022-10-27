1701deaa9f096e97c999b0284b2812aa397571a5
Nathan Shumoogum <nathan.shumoogum@methods.co.uk>
2022-10-27 16:47:54 +0100

    Add private get topic method to topic client

--- sdk/client_private.go
+++ sdk/client_private.go
@@ -30,6 +30,26 @@ func (cli *Client) GetRootTopicsPrivate(ctx context.Context, reqHeaders Headers)
 	return &rootTopics, nil
 }
 
+// GetTopicPrivate gets the full topic resource including the Next and Current nested objects
+func (cli *Client) GetTopicPrivate(ctx context.Context, reqHeaders Headers, id string) (*models.TopicResponse, apiError.Error) {
+	path := fmt.Sprintf("%s/topics/%s", cli.hcCli.URL, id)
+
+	b, apiErr := cli.callTopicAPI(ctx, path, http.MethodGet, reqHeaders, nil)
+	if apiErr != nil {
+		return nil, apiErr
+	}
+
+	var topic models.TopicResponse
+
+	if err := json.Unmarshal(b, &topic); err != nil {
+		return nil, apiError.StatusError{
+			Err: fmt.Errorf("failed to unmarshal topic - error is: %v", err),
+		}
+	}
+
+	return &topic, nil
+}
+
 // GetSubtopicsPrivate gets the private list of subtopics of a topic for Publishing which returns both Next and Current document(s) in the response
 func (cli *Client) GetSubtopicsPrivate(ctx context.Context, reqHeaders Headers, id string) (*models.PrivateSubtopics, apiError.Error) {
 	path := fmt.Sprintf("%s/topics/%s/subtopics", cli.hcCli.URL, id)
